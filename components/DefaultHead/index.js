fda5fd6b88377aa4dbc65f6a600ebfd202283c15
Chris Nunes <chrisnunes57@gmail.com>
2022-01-19 17:49:58 -0800

    added some seo stuff

--- components/DefaultHead/index.js
+++ components/DefaultHead/index.js
@@ -5,6 +5,14 @@ function DefaultHead() {
         <Head>
             <title>Wordle Stats</title>
             <meta property="og:title" content="Wordle Stats" key="title" />
+
+            <meta charSet="utf-8" />
+            <meta name="description" content="Check out the most common letter positions in Wordle" />
+            <meta name="author" content="Chris Nunes" />
+            <meta name="keywords" content="wordle letter position chris nunes" />
+
+            <meta property="og:url" content="https://wordle-stats-delta.vercel.app/" />
+            <meta property="og:image" content="/img/screencap.png" />
         </Head>
     )
 }
